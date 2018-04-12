c96f576cc34c1c45b41cf2533ece7d8cc724a55a
bob620 <bruder.kraft225@gmail.com>
2018-04-11 20:39:12 -0500

    updt(logger) basic logger

--- services/logger.js
+++ services/logger.js
@@ -1,10 +1,35 @@
-class Logger {
-	constructor() {
+const EventEmitter = require('events');
 
+const colors = require('colors');
+
+class Logger extends EventEmitter {
+	constructor() {
+		super();
+
+		this.data = {
+			services: new Map()
+		}
 	}
 
-	init() {
+	getLogs(serviceName, max=1) {
+		try {
+			const service = this.data.services.get(serviceName);
+			return service.slice(service.length() - max, service.length());
+		} catch(err) {return []}
+	}
 
+	log(serviceName, message) {
+		const service = this.data.services.get(serviceName);
+
+		if (service !== undefined)
+			service.push(message);
+		else
+			this.data.services.set(serviceName, [message]);
+		this.emit(serviceName, message);
+	}
+
+	createLogger(service) {
+		return this.log.bind(this, service);
 	}
 }
 
