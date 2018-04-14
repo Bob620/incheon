6812132b69f329447ac7ffdca367d8c4f4437574
bob620 <bruder.kraft225@gmail.com>
2018-04-13 19:50:53 -0500

    fix(database) made tests and fixed things

--- managers/databasemanager.js
+++ managers/databasemanager.js
@@ -39,20 +39,26 @@ class DatabaseManager {
 	}
 
 	setLocation(url) {
-		this.data.log ? this.data.log('Changing database location...') : {};
-		this.data.url = `mongodb://${url}`;
-		try {
-			this.data.connection.close();
-		} catch(err) {}
-
-		this.data.connection = MongoClient.connect(this.data.url, (err, client) => {
-			if (err) {
-				this.data.log ? this.data.log(err) : {};
-				throw new Error(err);
+		return new Promise((resolve, reject) => {
+			this.data.log ? this.data.log('Changing database location...') : {};
+			this.data.url = `mongodb://${url}`;
+			try {
+				this.data.connection.close();
+			} catch(err) {
 			}
 
-			this.data.log ? this.data.log(`Location changed to ${this.data.url}`) : {};
-			this.data.database = client.db(this.getDBName());
+			MongoClient.connect(this.data.url, (err, client) => {
+				if (err) {
+					this.data.log ? this.data.log(err) : {};
+					reject(err);
+					return;
+				}
+
+				this.data.log ? this.data.log(`Location changed to ${this.data.url}`) : {};
+				this.data.connection = client;
+				this.data.database = client.db(this.getDBName());
+				resolve();
+			});
 		});
 	}
 
@@ -66,7 +72,7 @@ class DatabaseManager {
 		this.data.log = log ? require('../services/logger').createLogger('database') : false;
 		this.setAccountInfo(account);
 		this.setDBName(dbName);
-		this.setLocation(url);
+		return this.setLocation(url);
 	}
 
 	/**
@@ -74,7 +80,7 @@ class DatabaseManager {
 	 * @param items {Object[]} One or more items to insert in the collection
 	 * @returns {Promise}
 	 */
-	insert(collectionName, ...items) {
+	insert(collectionName, items) {
 		if (this.isConnected() && collectionName && items.length > 0) {
 			switch(items.length) {
 				case 1:
@@ -92,12 +98,13 @@ class DatabaseManager {
 	/**
 	 * @param collectionName {String} Collection name in the database
 	 * @param query {Object} Query to use when searching the collection
+	 * @param limit {number} the max number to return
 	 * @returns {Promise}
 	 */
-	find(collectionName, query={}) {
+	find(collectionName, query={}, limit=0) {
 		if (this.isConnected() && collectionName) {
 			this.data.log ? this.data.log(`Searching in ${collectionName}`) : {};
-			return this.data.database.collection(collectionName).find(query).toArray();
+			return this.data.database.collection(collectionName).find(query).limit(limit).toArray();
 		}
 		this.data.log ? this.data.log('find failed') : {};
 		return Promise.reject(false);
@@ -156,7 +163,7 @@ class DatabaseManager {
 	deleteOne(collectionName, match) {
 		if (this.isConnected() && collectionName && match) {
 			this.data.log ? this.data.log(`Deleting one in ${collectionName}`) : {};
-			return this.data.database.collection(collectionName).deleteOne(match, item);
+			return this.data.database.collection(collectionName).deleteOne(match);
 		}
 		this.data.log ? this.data.log('deleteOne failed') : {};
 		return Promise.reject(false);
@@ -170,7 +177,7 @@ class DatabaseManager {
 	deleteMany(collectionName, match) {
 		if (this.isConnected() && collectionName && match) {
 			this.data.log ? this.data.log(`Deleting many in ${collectionName}`) : {};
-			return this.data.database.collection(collectionName).deleteMany(match, item);
+			return this.data.database.collection(collectionName).deleteMany(match);
 		}
 		this.data.log ? this.data.log('deleteMany failed') : {};
 		return Promise.reject(false);
--- tests/database.js
+++ tests/database.js
@@ -5,14 +5,90 @@ const options = {
 		"username": "accountUser",
 			"password": "password"
 	},
-	"log": true
+	"log": false
 };
 
 const logger = require('../services/logger');
 const database = require('../managers/databasemanager');
+const log = logger.createLogger('Tester');
 
 logger.on('message', (serviceName, message) => {
 	console.log(`[${serviceName}] - ${message}`)
 });
 
-database.init(options);
+database.init(options).then(async () => {
+	// Insert One Item
+	try {
+		const result = await database.insert('test', [{"test": [1]}]);
+		if (result.result.n === 1)
+			log(`${'PASS'.green} | Insert one`);
+		else
+			log(`${'FAIL'.red} | Insert one`);
+	} catch(err) {
+		log('Failed to insert one item: ');
+		log(err);
+	}
+
+	// Insert Many Items
+	try {
+		const result = await database.insert('test', [{"test": [2]}, {"test": [2]}, {"test": [3]}]);
+		if (result.result.n === 3)
+			log(`${'PASS'.green} | Insert many`);
+		else
+			log(`${'FAIL'.red}  | Insert many`);
+	} catch(err) {
+		log('Failed to insert many items: ');
+		log(err);
+	}
+
+	// Find One Item
+	try {
+		const result = await database.find('test', {test: {$in: [2]}}, 1);
+		if (result.length === 1)
+			log(`${'PASS'.green} | Find one`);
+		else
+			log(`${'FAIL'.red}  | Find one`);
+	} catch(err) {
+		log('Failed to find an item: ');
+		log(err);
+	}
+
+	// Find Many Items
+	try {
+		const result = await database.find('test', {test: {$in: [2]}}, 3);
+		if (result.length === 2)
+			log(`${'PASS'.green} | Find many`);
+		else
+			log(`${'FAIL'.red}  | Find many`);
+	} catch(err) {
+		log('Failed to find items: ');
+		log(err);
+	}
+
+	// Delete One Item
+	try {
+		const result = await database.deleteOne('test', {test: {$in: [1]}});
+		if (result.result.n === 1)
+			log(`${'PASS'.green} | Delete one`);
+		else
+			log(`${'FAIL'.red}  | Delete one`);
+	} catch(err) {
+		log('Failed to delete an item: ');
+		log(err);
+	}
+
+	// Delete Many Items
+	try {
+		const result = await database.deleteMany('test', {test: {$in: [1, 2, 3]}});
+		if (result.result.n === 3)
+			log(`${'PASS'.green} | Delete many`);
+		else
+			log(`${'FAIL'.red}  | Delete many`);
+	} catch(err) {
+		log('Failed to delete items: ');
+		log(err);
+	}
+}).catch((err) => {
+	log(err);
+});
+
